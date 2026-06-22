8c52b412c94e40b2ad28a4e71cced5648af018d7
monitor14 <monitor14.cai@gmail.com>
2026-06-23 00:16:28 +0800

    Day 2 learning JS : Var , typeof

---
+++ Day 2/script.js
@@ -0,0 +1,16 @@
+let name = "monitor14";
+let age = 28 ;
+let isOnline =true ;
+
+console.log(name ,age , isOnline);
+
+
+let str ="hello";
+let num = 42;
+let bool =  false;
+let nothing = null ; 
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof nothing);
