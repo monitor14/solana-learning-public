cf6ed8c39dcf6a3522eeea446b4d1f901d96059e
monitor14 <monitor14.cai@gmail.com>
2026-06-23 23:56:45 +0800

    Day 3 learn Var

---
+++ Day 3/script.js
@@ -0,0 +1,23 @@
+let name = "monitor14";
+let age = 28 ;
+let isOnline =true ;
+
+console.log(name ,age , isOnline);
+
+
+let str ="hello";
+let num = 42;
+let bool =  false;
+let nothing = null ; 
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof nothing);
+
+function greet(name){
+    return "hello," + name ;
+}
+
+let result = greet("Solana");
+console.log(result);
