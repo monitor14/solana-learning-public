9886305c24d211f0e9a0851ba6faa8e4ff4ecb3d
monitor14 <monitor14.cai@gmail.com>
2026-06-26 23:56:06 +0800

    Day 4: function scope, parameters, return values, naming

---
+++ Day 1 /Git basic.md
@@ -0,0 +1,5 @@
+占位符，之后再写完整思路
+1.  终端登录github
+2. 本地创建一个文件夹， 创建.gitgnore 文件， 忽略一些不用上传得文件
+3. github网站创建一个空repo
+4. 本地使用 git brach -m main , 切换主分支，git commit "话语"， 然后使用git push，把文件推送到github 
--- Day 3/script.js
+++ Day 3/script.js
@@ -1,23 +1,62 @@
-let name = "monitor14";
-let age = 28 ;
-let isOnline =true ;
+// let name = "monitor14";
+// let age = 28 ;
+// let isOnline =true ;
 
-console.log(name ,age , isOnline);
+// console.log(name ,age , isOnline);
 
 
-let str ="hello";
-let num = 42;
-let bool =  false;
-let nothing = null ; 
+// let str ="hello";
+// let num = 42;
+// let bool =  false;
+// let nothing = null ; 
 
-console.log(typeof str);
-console.log(typeof num);
-console.log(typeof bool);
-console.log(typeof nothing);
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof bool);
+// console.log(typeof nothing);
 
-function greet(name){
-    return "hello," + name ;
+// function greet(name){
+//     return "hello," + name ;
+// }
+
+// let result = greet("Solana");
+// console.log(result);
+
+// let globalName = "Solana";
+// function sayHi(){
+//     let localMsg = "Hi";
+//     console.log(localMsg+","+ globalName);
+// }
+
+// sayHi();
+// console.log(localMsg);
+
+// function greet(name="Guest"){
+//     return "Hello," + name;
+// }
+// console.log(greet("Solana"));
+// console.log(greet());
+
+
+// function checkAge(age){
+//     if (age >= 18) {
+//         return "成年";
+//     }
+//     return "未成年";
+// }
+
+// console.log(checkAge(20));
+// console.log(checkAge(15));
+
+function showPrimes(n) {
+  nextPrime: for (let i = 2; i < n; i++) {
+
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+
+    console.log(i); // 一个素数
+  }
 }
 
-let result = greet("Solana");
-console.log(result);
+console.log(showPrimes(10));
