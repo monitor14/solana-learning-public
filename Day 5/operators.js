6fe90311a2383c139be53acbbb20210bf4dfc07c
monitor14 <monitor14.cai@gmail.com>
2026-06-29 23:33:44 +0800

    Day 5: if-else, ternary operator, logical operators

---
+++ Day 5/operators.js
@@ -0,0 +1,10 @@
+let hasWallet = true ;
+let hasBalance = false ; 
+
+console.log(hasWallet && hasBalance);
+console.log(hasWallet || hasBalance);
+console.log(!hasWallet);
+
+let age = 20 ;
+let result = age >= 18 ? "Adult":"Minor";
+console.log(result);
